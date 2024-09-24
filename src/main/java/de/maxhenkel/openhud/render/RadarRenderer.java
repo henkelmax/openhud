e3b68c65120a0f71077d00b8b3008c211883777f
Max Henkel <max@maxhenkel.de>
2024-09-24 11:54:14 +0200

    Add waypoint rendering

--- src/main/java/de/maxhenkel/openhud/render/RadarRenderer.java
+++ src/main/java/de/maxhenkel/openhud/render/RadarRenderer.java
@@ -1,10 +1,13 @@
 package de.maxhenkel.openhud.render;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import de.maxhenkel.openhud.waypoints.Waypoint;
+import de.maxhenkel.openhud.waypoints.WaypointClientManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.FastColor;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix4f;
 
 public class RadarRenderer {
@@ -58,6 +61,28 @@ public class RadarRenderer {
         drawString(guiGraphics, contentStartX, contentStartY, contentWidth, contentHeight, east, "E");
         float southeast = calculateDirection(315F);
         drawLine(guiGraphics, contentStartX, contentStartY, contentWidth, contentHeight, SHORT_LINE_HEIGHT, southeast);
+
+        Vec3 cameraPos = mc.gameRenderer.getMainCamera().getPosition();
+        double cameraX = cameraPos.x;
+        double cameraZ = cameraPos.z;
+        for (Waypoint waypoint : WaypointClientManager.getWaypoints().getWaypoints()) {
+            float waypointPos = calculateDirection(getWaypointAngle(waypoint, cameraX, cameraZ));
+            drawLine(guiGraphics, contentStartX, contentStartY, contentWidth, contentHeight, LINE_HEIGHT, waypointPos);
+            drawString(guiGraphics, contentStartX, contentStartY, contentWidth, contentHeight, waypointPos, "X");
+        }
+    }
+
+    public static float getWaypointAngle(Waypoint waypoint, double cameraX, double cameraZ) {
+        double waypointX = waypoint.getPosition().getX() + 0.5D;
+        double waypointZ = waypoint.getPosition().getZ() + 0.5D;
+        // Calculate dx and dz
+        double dx = waypointX - cameraX;
+        double dz = waypointZ - cameraZ;
+        // Calculate the angle in radians
+        double angleInRadians = Math.atan2(dx, dz);
+
+        // Normalize the angle to range [0, 360)
+        return (float) ((-Math.toDegrees(angleInRadians) + 360D) % 360D);
     }
 
     private static void drawLine(GuiGraphics guiGraphics, float hudX, float hudY, float hudWidth, float hudHeight, float lineHeight, float perc) {
