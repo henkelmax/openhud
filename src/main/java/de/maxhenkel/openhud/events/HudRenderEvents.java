117f3d93ca835878e34f92fba6d8c6245b83802f
Max Henkel <max@maxhenkel.de>
2024-09-25 22:10:41 +0200

    Hide HUD when GUI is hidden

--- src/main/java/de/maxhenkel/openhud/events/HudRenderEvents.java
+++ src/main/java/de/maxhenkel/openhud/events/HudRenderEvents.java
@@ -2,6 +2,7 @@ package de.maxhenkel.openhud.events;
 
 import de.maxhenkel.openhud.Main;
 import de.maxhenkel.openhud.render.RadarRenderer;
+import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -13,8 +14,13 @@ import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 @EventBusSubscriber(Dist.CLIENT)
 public class HudRenderEvents {
 
+    private static final Minecraft mc = Minecraft.getInstance();
+
     @SubscribeEvent
-    public static void onRenderGuiLayer(RenderGuiLayerEvent.Post event) {
+    public static void onRenderGuiLayer(RenderGuiLayerEvent.Pre event) {
+        if (mc.options.hideGui) {
+            return;
+        }
         if (!VanillaGuiLayers.HOTBAR.equals(event.getName())) {
             return;
         }
